c33b4b3ccc8e049f00bc8627389bb9bd3c485faa
Alain D'EURVEILHER <alain.deurveilher@infrabel.be>
2021-11-23 03:53:16 +0100

    Fix migration

--- migrations/20211108222021-products-in-orders.js
+++ migrations/20211108222021-products-in-orders.js
@@ -19,7 +19,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20211108222021-products-to-orders-up.sql');
+  var filePath = path.join(__dirname, 'sqls', '20211108222021-products-in-orders-up.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
@@ -34,7 +34,7 @@ exports.up = function(db) {
 };
 
 exports.down = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20211108222021-products-to-orders-down.sql');
+  var filePath = path.join(__dirname, 'sqls', '20211108222021-products-in-orders-down.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
